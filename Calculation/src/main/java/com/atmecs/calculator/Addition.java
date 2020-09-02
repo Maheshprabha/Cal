e8ebec2ff06c7359fbc2be527e8bd1e09ba74f36
admin <admin@DESKTOP-ATOQJFI>
2020-09-01 21:56:59 -0700

    Calc

--- Calculation/src/main/java/com/atmecs/calculator/Addition.java
+++ Calculation/src/main/java/com/atmecs/calculator/Addition.java
@@ -1,27 +1,23 @@
 package com.atmecs.calculator;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Addition {
-	 @SuppressWarnings({ "rawtypes", "resource" })
+
 	public void addingList() {
 		 //To get the input from user
-		 ArrayList<List>arrlist = new ArrayList<List>(); 
+		 ArrayList<Integer>arrlist = new ArrayList<Integer>(); 
 		Scanner scan = new Scanner(System.in);
-		 System.out.println("Enter the size of the array :");
-		int size = scan.nextInt();
-		for(int i=0; i<=size;i++) {
-			System.out.println("enter the input");
-			@SuppressWarnings("unused")
-			int data = scan.nextInt();
-			arrlist.add(arrlist);
-		    System.out.println(arrlist);
-
+		 //System.out.println("Enter the size of the array :");
+		System.out.println("Enter the elements :");
+		while(scan.hasNextInt()) {
+			arrlist.add(scan.nextInt());
+		}
+System.out.println(arrlist);
 			
 			 
 		 }
-	 }
+	 
 
 }
